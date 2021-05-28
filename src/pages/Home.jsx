678f25976ac9b4a4605126ec272623fe2e9396ff
Pankaj Sogan <81852236+pankajsogan@users.noreply.github.com>
2021-05-28 21:17:59 +0530

    message count added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -153,7 +153,9 @@ return browserName;
                 <img alt="Instagram" class="s4Iyt" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" srcset="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png 2x"/>
                 </div>
                 <div className="navbar__mobile__right">
-                    <button><MessageIcon/></button>
+                    <button>
+                        <span className="message__count">0</span>
+                        <MessageIcon/></button>
                 </div>
             </div>
            <div className="body__container" onClick={()=>props.drop && props.setDrop(false)}>
