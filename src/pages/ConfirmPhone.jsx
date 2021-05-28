2434ad4322eeed8918f8aa0ddf9bf97b6f106d36
Pankaj Sogan <81852236+pankajsogan@users.noreply.github.com>
2021-05-28 14:59:32 +0530

    responseive added

--- src/pages/ConfirmPhone.jsx
+++ src/pages/ConfirmPhone.jsx
@@ -2,8 +2,44 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import "./ConfirmPhone.css"
 import PhoneIcon from '@material-ui/icons/Phone';
+import axios from 'axios';
+import { setUser } from '../actions/userAction';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router';
 
-function ConfirmPhone() {
+function ConfirmPhone(props) {
+
+const [phone,setPhone] =React.useState('');
+    const handlePhoneAdd = async ()=>{
+        try{
+            const r = await axios.put(`http://localhost:5000/auth/add/phone`,{phone:phone,uid:props.user && props.user.id});
+            return r.data;
+        }
+        catch(e){
+            if(e.response && e.response.data){
+                return e.response.data;
+            }
+        }
+    }
+
+
+    const history = useHistory();
+
+    const handlePhone = ()=>{
+        const mobile_rex = /^(\+|\d)[0-9]{7,16}$/;
+       if(mobile_rex.test(phone)){
+        handlePhoneAdd().then((data)=>{
+            if(!data.code){
+                return alert(data.message);
+            }
+            history.push('/');
+            
+        })
+       }
+       else{
+           alert("Not a Valid Phone Number");
+       }
+    }
     return (
         <div className="confirmPhone">
             <Navbar/>
@@ -17,10 +53,10 @@ function ConfirmPhone() {
                     <p>Adding your number will help you log in more easily, recover your account, and find people to follow.</p>
                     <div className="form__account__setup">
                         <div className="form__control__phone">
-                            <input type="text" name="phone" id="phone" />
+                            <input type="text" name="phone" id="phone" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
                             
                         </div>
-                        <input type="submit" value="Add phone number" />
+                        <input type="button" value="Add phone number" onClick={handlePhone}/>
                             <p>You may receive an email from Reactgram and can opt out at anytime</p>
                     </div>
                     </div>
@@ -30,5 +66,14 @@ function ConfirmPhone() {
         </div>
     )
 }
+const mapStateToProps = (state) =>({
+    user: state.userReducer.user,
+})
 
-export default ConfirmPhone
+const mapDispatchToProps = (dispatch)=>({
+    setUser:(user)=>dispatch(setUser(user)),
+})
+
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(ConfirmPhone)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import "./Home.css"
 import PhoneIcon from '@material-ui/icons/Phone';
 import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import Post from '../components/Post';
+import AddIcon from '@material-ui/icons/Add';
 
 function Home(props) {
 
@@ -71,7 +72,7 @@ return browserName;
         }
         async function getUser() {
                 try{
-                    const r = await axios.get('https://secure-woodland-04703.herokuapp.com/auth/user',{headers:{
+                    const r = await axios.get('http://localhost:5000/auth/user',{headers:{
                         "Authorization":"Bearer " +Cookies.get("AUTH_TOKEN"),
                         "Content-Type":"application/json"
                     }})
@@ -109,7 +110,7 @@ return browserName;
             getLocation().then((data)=>{
                 console.log(data);
                 try{
-                    const r = axios.put("https://secure-woodland-04703.herokuapp.com/auth/updateActivity",{data,browser:browser && browser,email: props.user && props.user.email});
+                    const r = axios.put("http:://localhost:5000/auth/updateActivity",{data,browser:browser && browser,email: props.user && props.user.email});
                     console.log(r.data)
                 }
                 catch(e){
@@ -132,9 +133,26 @@ return browserName;
     const handleProfileNav =()=>{
         history.push(`/${props.user && props.user.user}/profile`);
     }
+
+
+    function MessageIcon(){
+        return <svg aria-label="Messenger" class="_8-yf5 " fill="#262626" height="22" viewBox="0 0 48 48" width="22"><path d="M36.2 16.7L29 22.2c-.5.4-1.2.4-1.7 0l-5.4-4c-1.6-1.2-3.9-.8-5 .9l-6.8 10.7c-.7 1 .6 2.2 1.6 1.5l7.3-5.5c.5-.4 1.2-.4 1.7 0l5.4 4c1.6 1.2 3.9.8 5-.9l6.8-10.7c.6-1.1-.7-2.2-1.7-1.5zM24 1C11 1 1 10.5 1 23.3 1 30 3.7 35.8 8.2 39.8c.4.3.6.8.6 1.3l.2 4.1c0 1 .9 1.8 1.8 1.8.2 0 .5 0 .7-.2l4.6-2c.2-.1.5-.2.7-.2.2 0 .3 0 .5.1 2.1.6 4.3.9 6.7.9 13 0 23-9.5 23-22.3S37 1 24 1zm0 41.6c-2 0-4-.3-5.9-.8-.4-.1-.8-.2-1.3-.2-.7 0-1.3.1-2 .4l-3 1.3V41c0-1.3-.6-2.5-1.6-3.4C6.2 34 4 28.9 4 23.3 4 12.3 12.6 4 24 4s20 8.3 20 19.3-8.6 19.3-20 19.3z"></path></svg>
+    }
     return (
         <div className="home__container">
+
+            <button className="new_post_btn"><AddIcon/> <span>New Post</span></button>
             <Navbar/>
+
+            <div className="navbar__mobile">
+                <div className="navbar__mobile__left"><button><AddIcon/></button></div>
+                <div className="navbar__mobile__center">
+                <img alt="Instagram" class="s4Iyt" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" srcset="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png 2x"/>
+                </div>
+                <div className="navbar__mobile__right">
+                    <button><MessageIcon/></button>
+                </div>
+            </div>
            <div className="body__container" onClick={()=>props.drop && props.setDrop(false)}>
            <div className="blank"></div>
             <div className="home__body">
@@ -142,9 +160,9 @@ return browserName;
                 <div className="posts">
                     {/* <Stories/> */}
                     <div className="getting__started">
-                       <div className="getting__started__header">
+                      {props.user!=null ? !props.user.phone &&  <div className="getting__started__header">
                            Getting Started
-                       </div>
+                       </div>:null}
 
                        <div className="start__cards">
                        {props.user && !props.user.phone && <StartCard title="Add Phone Number" subtitle="Add your phone number so you can reset your password, find friends and more." button="Add Phone Number" Icon={PhoneIcon} link="/accounts/confirm-phone"/>}
