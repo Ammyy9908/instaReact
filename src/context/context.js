625bf4c142f2d2b0f54cf1daf5984d2337b9cbde
Ammyy9908 <sb78639@gmail.com>
2021-07-06 07:55:13 +0530

    socket fixed at client side

--- src/context/context.js
+++ src/context/context.js
@@ -7,5 +7,5 @@ dotenv.config();
 
 //process.env.REACT_APP_SERVER_PROD
 //https://whatrooms.herokuapp.com
-export const socket = Cookies.get("AUTH_TOKEN") && socketio(`https://secure-woodland-04703.herokuapp.com/ `,{ transports: ["websocket"] });
+export const socket = Cookies.get("AUTH_TOKEN") && socketio(`https://secure-woodland-04703.herokuapp.com`,{ transports: ["websocket"] });
 export const SocketContext = React.createContext();
